f91998e05b3c25b93a2aabc903791f7371b88a2d
Pabloblockchain14 <pablo.arce.p@mail.pucv.cl>
2023-09-13 00:41:13 -0300

    preEntrega finalizada

--- src/routes/carts.router.js
+++ src/routes/carts.router.js
@@ -10,35 +10,33 @@ function crearId(){
 
 router.post("/api/carts", (req,res) => {
     let carts = JSON.parse(fs.readFileSync("carrito.json", "utf-8"));
-    const newCart = {
-        id:crearId(),
-        products:[]
-    }
-    carts.push(newCart)
+    carts.push({id:crearId(),products:[]})
     fs.writeFileSync("carrito.json", JSON.stringify(carts));
     res.json({message: "Carrito agregado"})
 })
 
 router.get("/api/carts/:cid", (req,res) =>{
     let carts = JSON.parse(fs.readFileSync("carrito.json", "utf-8"));
-    const cart = carts.find(car => car.id.toString() == (req.params.cid))
-    !cart ?  res.send("Id ingresado no existe") : res.json(cart.productosCarrito)
+    const cart = carts.find(car => car.id == (req.params.cid))
+    !cart ?  res.send("Id ingresado no existe") : res.json(cart.products)
 })
 
 router.post("/api/carts/:cid/products/:pid", (req,res) =>{
     let carts = JSON.parse(fs.readFileSync("carrito.json", "utf-8"));
-
+    let stockProducts = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
     const carritoEncontrado = carts.find(carrito => carrito.id.toString() === (req.params.cid))
+    const productoExiste = stockProducts.find(producto => producto.id.toString() === (req.params.pid))
     if (!carritoEncontrado){
         return res.send("Carrito ingresado no existe")
-        
+    }
+    if (!productoExiste) {
+        return res.send("Producto ingresado no existe")
     }
     const productoEncontrado = carritoEncontrado.products.find( producto => producto.id === req.params.pid )
     productoEncontrado ? productoEncontrado.quantity++ : carritoEncontrado.products.push( {id:req.params.pid, quantity:1})
     fs.writeFileSync("carrito.json", JSON.stringify(carts));
     res.json(carts)
 })
-
 
 router.get("/api/carts", (req,res) => {
     let carts = JSON.parse(fs.readFileSync("carrito.json", "utf-8"));
--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -2,7 +2,11 @@ const express = require("express")
 const fs = require("fs");
 const router = express.Router()
 
-
+function crearId(){
+    const timestamp = Date.now();
+    const uniqueID = `${timestamp}-${Math.floor(Math.random() * 10000)}`;
+    return uniqueID
+}
 
 router.get("/api/products", (req,res) => {
         const products = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
@@ -16,28 +20,31 @@ router.get("/api/products/:pid",(req,res) =>{
     !producto ? res.send("Producto no existe") : res.send(producto)
 })
 
-function crearId(){
-    const timestamp = Date.now();
-    const uniqueID = `${timestamp}-${Math.floor(Math.random() * 10000)}`;
-    return uniqueID
-}
-
-    // falta afinar la validacion de que los campos hayan sido ingresados.
-
 router.post("/api/products", (req,res) => {
     let products = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
     const newProduct = req.body
+
+    if ( 
+    !newProduct.title ||
+    !newProduct.description ||
+    !newProduct.code ||
+    !newProduct.price ||
+    !newProduct.status ||
+    !newProduct.stock ||
+    !newProduct.category
+    ){
+        return res.send("Faltan datos por ingresar")
+    }
+
     newProduct.id = crearId()
     products.push(newProduct)
     fs.writeFileSync("productos.json", JSON.stringify(products));
     res.json({products})
 })
 
-
 router.put("/api/products/:pid", (req,res) => {
     let products = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
     let idEdit = products.findIndex( producto => producto.id.toString() === req.params.pid)
-        
     if (idEdit !== -1){
         const productoEditado = req.body
         productoEditado.id = req.params.pid
@@ -48,6 +55,7 @@ router.put("/api/products/:pid", (req,res) => {
         res.send("Producto no existe")
     }
 })
+
 router.delete("/api/products/:pid", (req,res) => {
     let products = JSON.parse(fs.readFileSync("productos.json", "utf-8"));
     const productoEncontrado = products.find((producto) => producto.id.toString() === req.params.pid)
@@ -55,7 +63,6 @@ router.delete("/api/products/:pid", (req,res) => {
         products = products.filter((producto) => producto.id.toString() !== req.params.pid)
         fs.writeFileSync("productos.json", JSON.stringify(products));
         res.json({products})
-
     }else{
         res.send("Producto no existe")
     }
